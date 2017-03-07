c347082e84458a7247d5aa51e1adbeb5d0406fc2
Gustavo Cardoso <cardosogustavo02@gmail.com>
2017-03-07 00:17:38 -0500

    Creating Order

--- willylist/urls.py
+++ willylist/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from willylistapp import views
 from django.contrib.auth import views as auth_views
-
 from django.conf.urls.static import static
 from django.conf import settings
+
+from willylistapp import views, apis
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,12 +24,20 @@ urlpatterns = [
     url(r'^restaurant/account/$', views.restaurant_account, name = 'restaurant-account'),
     url(r'^restaurant/meal/$', views.restaurant_meal, name = 'restaurant-meal'),
     url(r'^restaurant/meal/add/$', views.restaurant_add_meal, name = 'restaurant-add-meal'),
-    url(r'^restaurant/meal/edit/(?P<meal_id>\d+)/$', views.restaurant_edit_meal, name = 'restaurant-edit-meal'),
+    url(r'^restaurant/meal/edit/(?P<meal_id>\d+)/$', views.restaurant_edit_meal, name = 'restaurant-edit-meal'), # ?P<meal_id>\d+) is passing meal_id parameter inside meal
     url(r'^restaurant/order/$', views.restaurant_order, name = 'restaurant-order'),
     url(r'^restaurant/report/$', views.restaurant_report, name = 'restaurant-report'),
 
-    # Sign In / Sign Up / Sign Out
-    url(r'^api/social/', include('rest_framework_social_oauth2.urls')), #we'll use api but we can use other api's
-    # //convert-token (sign in / sign out)
+    # Sign In/ Sign Up/ Sign Out
+    url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
+    # /convert-token (sign in/ sign up)
     # /revoke-token (sign out)
+     url(r'^api/restaurant/order/notification/(?P<last_request_time>.+)/$', apis.restaurant_order_notification),
+
+    # APIs for CUSTOMERS
+    url(r'^api/customer/restaurants/$', apis.customer_get_restaurants),
+    url(r'^api/customer/meals/(?P<restaurant_id>\d+)/$', apis.customer_get_meals),
+    url(r'^api/customer/order/add/$', apis.customer_add_order),
+    url(r'^api/customer/order/latest/$', apis.customer_get_latest_order),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- willylistapp/admin.py
+++ willylistapp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from willylistapp.models import Restaurant, Customer, Driver, Meal
+from willylistapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
 
 admin.site.register(Restaurant) # add Restaurant object into admin.site
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
